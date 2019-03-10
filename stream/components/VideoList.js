075bb9ef784ba2559b653703308b88a22ff01033
JayprakashDas <47481071+JayprakashDas@users.noreply.github.com>
2019-03-10 22:17:00 +0530

    Update VideoList.js

--- stream/components/VideoList.js
+++ stream/components/VideoList.js
@@ -3,7 +3,7 @@ import VideoItem from './VideoItem';
 
 
 const Videolist = ({videos, onVideoselect})=>{
-	//videos array passed from the App
+	
 	const renderList = videos.map((video)=>{
 		return <VideoItem key={video.id.videoId} onVideoselect={onVideoselect} video={video}/>
 	});
